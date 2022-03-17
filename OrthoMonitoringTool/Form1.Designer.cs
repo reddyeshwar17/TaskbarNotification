b10f29b090148b8060cee547bd29041bc5e17ddb
Eswar Reddy <dr00684790@techmahindra.com>
2022-03-17 15:13:44 +0530

    'NotifyIcon'

---
+++ OrthoMonitoringTool/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+using OrthoMonitoringTool.Properties;
+
+namespace OrthoMonitoringTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent(bool isStopped)
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Notify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Notify
+            // 
+            this.Notify.ContextMenuStrip = this.contextMenuStrip1;
+            if (!isStopped)
+            {
+                this.Notify.Icon = ((System.Drawing.Icon)(Resources.Blue));
+                Notify.Text = "Ortho started";
+            }
+            else
+            {
+                this.Notify.Icon = ((System.Drawing.Icon)(Resources.Red));
+                Notify.Text = "Ortho stopped";
+            }
+            this.Notify.Text = "Notify";
+            this.Notify.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Notify_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.restartToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(111, 70);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Ortho monitoring";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon Notify;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+    }
+}
+
---
+++ OrthoMonitoringTool/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.DirectoryServices;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using System.Windows.Forms;
+using Microsoft.Web.Administration;
+using Microsoft.Win32;
+using OrthoMonitoringTool.Properties;
+
+namespace OrthoMonitoringTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            bool isStopped = StartStopOrtho();
+            InitializeComponent(isStopped);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bool isConFailed = GetOrthoMiddlewareConnection();
+            bool isStopped = StartStopOrtho(isConFailed);
+            SetMessage(isStopped);
+            Notify.BalloonTipTitle = "Ortho";
+
+
+            /*ServerManager mgr = new ServerManager();
+            string SiteName = HostingEnvironment.SiteName;
+            Site currentSite = mgr.Sites[SiteName];
+
+            //The following obtains the application name and application object
+            //The application alias is just the application name with the "/" in front
+
+            string ApplicationAlias = HostingEnvironment.ApplicationVirtualPath;
+            string ApplicationName = ApplicationAlias.Substring(1);
+            Microsoft.Web.Administration.Application app = currentSite.Applications[ApplicationAlias];
+            */
+        }
+
+        private bool GetOrthoMiddlewareConnection()
+        {
+            bool isFailed = false;
+            IEnumerable<string> conection = File.ReadAllLines(@"D:\Ortho\MiddlewareCon.txt").Where(x => x.Contains("failed"));
+
+            foreach (var item in conection)
+            {
+                isFailed = true;
+            }
+            // Open the text file using a stream reader.
+            //using (var sr = new StreamReader("TestFile.txt"))
+            //{
+            //    // Read the stream as a string, and write the string to the console.
+            //    Console.WriteLine(sr.ReadToEnd());
+            //}
+            return isFailed;
+        }
+        private void SetMessage(bool isStopped)
+        {
+            if (!isStopped)
+            {
+                Notify.BalloonTipText = "Ortho application started successfully";
+                this.Notify.Icon = Resources.Blue;
+                Notify.Text = "Ortho started";
+                Notify.ShowBalloonTip(10000);
+            }
+            else
+            {
+                Notify.BalloonTipText = "Ortho application stopped successfully";
+                this.Notify.Icon = Resources.Red;
+                Notify.Text = "Ortho stopped";
+
+                Notify.ShowBalloonTip(10000);
+            }
+        }
+
+        private bool StartStopOrtho(bool startOrtho = false)
+        {
+            bool isStopped = false;
+            ServerManager serverManager = new ServerManager();
+            var sites1 = serverManager.Sites[0].Applications[0].pa;
+            var sites = serverManager.Sites;
+            var pool = serverManager.ApplicationDefaults.ApplicationPoolName;
+            foreach (var site in sites)
+            {
+                if (site.State == ObjectState.Stopped && startOrtho)
+                {
+                    site.Start();
+                    isStopped = false;
+                }
+                else if (site.State == ObjectState.Started && !startOrtho)
+                {
+                    site.Stop();
+                    isStopped = true;
+                }
+                else if (site.State == ObjectState.Stopped)
+                {
+                    isStopped = true;
+                }
+            }
+            return isStopped;
+        }
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            var eventArgs = e as MouseEventArgs;
+            switch (eventArgs.Button)
+            {
+                // Left click to reactivate
+                case MouseButtons.Left:
+                    // Do your stuff
+                    break;
+            }
+        }
+
+        private void Notify_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            Notify.Visible = true;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                Notify.Visible = true;
+                Notify.ShowBalloonTip(10000);
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+                Notify.Visible = false;
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool isStopped = StartStopOrtho(true);
+            SetMessage(isStopped);
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool isStopped = StartStopOrtho(false);
+            SetMessage(isStopped);
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool isStarted = StartStopOrtho(true);
+            bool isStarted1 = StartStopOrtho(false);
+            if (isStarted1)
+            {
+                Notify.BalloonTipTitle = "Ortho app restarted successfully";
+                this.Notify.Icon = Resources.Blue;
+            }
+        }
+    }
+
+}
